d5b4859729bb935d3f2714647c4919f41d3e1212
mikeBridge1993 <miguelponte93@hotmail.com>
2018-02-14 18:05:27 -0100

    Add ip feature

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const moment = require('moment');
 const useragent = require('express-useragent');
 const port = process.env.PORT || 3000;
-
+const requestIp = require('request-ip');
 var app = express();
 
 app.use(useragent.express());
+
 app.get('/', (req, res) =>{
     
     result = {
-        ip: req.connection.remoteAddress,
+        ip:  req.headers['x-forwarded-for'] || req.connection.remoteAddress,
         language: req.headers["accept-language"].split(',')[0],
     }
     
